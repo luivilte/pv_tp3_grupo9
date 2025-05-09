8409ed36f646ad0d592522bc23fe66f88d11b9c7
luivilte <vilteluis665@gmail.com>
2025-05-09 09:36:16 -0300

    TaskList

---
+++ pv_tp3_grupo_9/src/components/TaskList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TaskList = () => {
+return (
+    <div>
+        <TaskInput settar={settar} tareas={tareas}/>
+        <ul>
+        <TaskItem settar={settar} tareas={tareas}/>
+        </ul>
+    </div>
+  )
+}
