627d9f414197e637241c178d3affc9c91f0406d4
MajoGT2015 <morguapos2015@gmail.com>
2025-05-11 00:16:57 -0300

    TaskList.jsx

--- pv_tp3_grupo_9/src/components/TaskList.jsx
+++ pv_tp3_grupo_9/src/components/TaskList.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
 
-export const TaskList = () => {
-return (
-    <div>
-        <TaskInput settar={settar} tareas={tareas}/>
-        <ul>
-        <TaskItem settar={settar} tareas={tareas}/>
-        </ul>
-        
-    </div>
-  )
-}
+import React from "react";
+import TaskItem from "./TaskItem"; 
+
+const TaskList = ({ tareas, settar }) => {
+  if (!tareas || tareas.length === 0) return <p>No hay tareas aún.</p>;
+
+  return (
+    <ul>
+      {tareas.map((tarea) => (
+        <TaskItem key={tarea.id} tar={tarea} tareas={tareas} settar={settar} />
+      ))}
+    </ul>
+  );
+};
+
+export default TaskList;
