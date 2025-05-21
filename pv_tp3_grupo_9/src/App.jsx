8cf84f3ece1974338908b6ec6458d31d7cd119e3
ValerianoHJ <valerianohernanj@gmail.com>
2025-05-21 13:38:41 -0300

    agrego app.jsx

--- pv_tp3_grupo_9/src/App.jsx
+++ pv_tp3_grupo_9/src/App.jsx
@@ -1,18 +1,45 @@
 
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import TaskList from './components/TaskList'
+import TaskInput from './components/TaskInput'
+
 
 function App() {
  
+      const [tasks, setTasks] = useState([]);
 
+      const addTask = (text) => {
+        const newTask = { id: Date.now(), text, completed: false};
+        setTasks([...tasks, newTask]);
+      };
+      const toggleTask = (id) => {
+        setTasks(tasks.map(task => 
+          task.id === id ? {...task, completed: !task.completed}:task
+        ));
+      };
+      const deleteTask = (id) => {
+        setTasks(tasks.filter(task => task.id !== id));
+      };
 
   return (
-    
-        <TaskList/>
-  )
+
+    <div style={{
+      maxWidth: "600px",
+      margin: "40px auto",
+      padding: "20px",
+      background: "#fff",
+      borderRadius: "10px",
+      boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+    }}
+    >
+
+    <h1 style={{ frontSize: "28px", frontWeight: "bold" }}>Lista de Tareas</h1>
+      <TaskInput onAddTask={addTask}/>
+      <TaskList tasks={tasks}
+      onToggle={toggleTask} onDelete={deleteTask}/>
+    </div>
+  );
 }
 
 export default App
